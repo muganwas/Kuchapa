64e95a924a4d442b5b3bd69ec0cbd9f559a0826f
Muganwa <stevenmuganwa@live.com>
2023-08-10 15:56:04 +0300

    index cleanup

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { Provider } from 'react-redux';
 import { AppRegistry } from 'react-native';
 import App from './src/components/SplashScreen';
 import { name as appName } from './app.json';
 import configureStore from './store';
-import { Provider } from 'react-redux';
 
 const store = configureStore();
 
